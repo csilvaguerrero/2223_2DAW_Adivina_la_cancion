6aecf7ca348c29f676fdb266cc9a98ec5942c052
Daniel Carvajal <juandanielcarvajalmontes.guadalupe@alumnado.fundacionloyola.net>
2022-11-27 18:22:19 +0100

    Añadidas vistas CRUD, login administrador y plan de pruebas

--- src/php/vistas/inicio.php
+++ src/php/vistas/inicio.php
@@ -5,6 +5,8 @@
                     <div>
                         <h1>ADIVINA LA CANCIÓN</h1>
                     </div>
+                    <a href="../../html/crudcanciones.html">Gestiónar canciones</a>
+                    <a href="../../html/crudtemas.html">Gestiónar temas de lucha</a>
                     <div id="admin">
                         <span>'.$_SESSION['admin']['nombre'].'</span>
                         <a href="logout.php">Cerrar sesión</a>
