6f015de7ea7f9c8acf0a708d5137c30dcc193cc3
Mario Pérez <mperezgarcia.guadalupe@alumnado.fundacionloyola.net>
2022-12-02 10:30:15 +0100

    validacion impedir valores especiales en las respuestas

--- src/js/validaciones/validacion.js
+++ src/js/validaciones/validacion.js
@@ -9,3 +9,13 @@ function validar() {
       alert('Archivo invalido. No se permite la extension ' + extension);
     }
   }
+  function NumText(string){//solo letras y numeros
+    var out = '';
+    //Se añaden las letras validas
+    var filtro = 'abcdefghijklmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ1234567890';//Caracteres validos
+	
+    for (var i=0; i<string.length; i++)
+       if (filtro.indexOf(string.charAt(i)) != -1) 
+	     out += string.charAt(i);
+    return out;
+}
