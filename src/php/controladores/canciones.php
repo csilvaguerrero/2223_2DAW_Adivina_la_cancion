60d00c417219e757e34f7fbca237b5d86a23d15d
Cristian Silva <csilvaguerrero.guadalupe@alumnado.fundacionloyola.net>
2022-12-02 11:00:23 +0100

    Version final PHP MVC

--- src/php/controladores/canciones.php
+++ src/php/controladores/canciones.php
@@ -1,19 +1,34 @@
 <?php
-
+    /**
+     * @file Clase que gestiona los procesos relacionados con las canciones (CRUD)
+     **/
     class Canciones extends Controlador{
+        /**
+         * Constructor de la clase
+         **/
         function __construct(){
             parent::__construct();
             $this->cargarModelo('cancionModelo');
             $this->mostrarVista();
         }
-
+        /**
+         * Clase que muestra la vista con todas las canciones registradas
+         **/
         public function mostrarVista(){
             $resultado = $this->modelo->consultarCanciones();
             include 'php/vistas/vistaCanciones.php';
         }
+
+        /**
+         * Clase que elimina canciones registradas en la base de datos.
+         **/
         public function borrarFila($id){
             $this->modelo->borrarCanciones($id);
         }
+
+        /**
+         * Clase que añade canciones mediante un formulario a la base de datos.
+         **/
         public function anadirFila(){
 
             $audio = $_FILES['audio']['name'];
@@ -25,7 +40,6 @@
             $id = $this->modelo->anadirCanciones($audio, $titulo, $categoria, $radio, $temporal);
             $this->modelo->anadirRespuestas($id, $radio, $titulo);
 
-           // header('Location: https://01.2daw.esvirgua.com/adivina-cancion/canciones');
         }
     }
 
--- src/php/modelos/cancionModelo.php
+++ src/php/modelos/cancionModelo.php
@@ -1,27 +1,45 @@
 <?php
-
+    /**
+     * @file Clase que gestiona el modelo del controlador de las canciones
+     **/
     class cancionModelo extends Modelo{
+        /**
+         * Constructor de la clase
+         **/
         public function __construct(){
             parent::__construct();
         }
-
+        /**
+         * Método que consulta los datos de las canciones en la base de datos
+         * @return {object} $resultado Objeto que contiene el resultado de la consulta
+         **/
         public function consultarCanciones(){
             $sql = "SELECT AC_canciones.id, AC_respuestas.titulo, AC_categorias.nombre FROM AC_categorias INNER JOIN AC_canciones ON AC_canciones.id_categoria = AC_categorias.id
                     INNER JOIN AC_respuestas ON AC_respuestas.num_respuesta = AC_canciones.respuesta_correcta AND AC_canciones.id = AC_respuestas.id_cancion;";
             $resultado = $this->conexion->query($sql);
             return $resultado;
         }
+        /**
+         * Método que borra las filas de las canciones en la base de datos.
+         **/
         public function borrarCanciones($id){
             $sql = "DELETE FROM AC_canciones WHERE id = $id;";
             $this->conexion->query($sql);
         }
+        /**
+         * Método que añade las canciones en la base de datos.
+         * @return {object} $id de la última consulta realizada.
+         **/
         public function anadirCanciones($audio, $titulo, $categoria, $radio, $temporal){
             $sql = "INSERT INTO AC_canciones(audio, respuesta_correcta, id_categoria) VALUES ('$audio',$radio,$categoria)";
             $this->conexion->query($sql);
             $id = $this->conexion->insert_id;
-            //$this->modelo->move_uploaded_file($temporal, 'audio/'.$titulo);
+            //$this->move_uploaded_file($temporal, 'audio/'.$titulo);
             return $id;
         }
+        /**
+         * Método que añade respuestas a las canciones añadidas por el usuario.
+         **/
         public function anadirRespuestas($id, $radio, $titulo){
             $sql = "INSERT INTO AC_respuestas(id_cancion, num_respuesta, titulo) VALUES($id, $radio, '$titulo')";
             $this->conexion->query($sql);
